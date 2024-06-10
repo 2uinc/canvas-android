dca46b687d600ae83314f450d54f56c2360da330
k1rill <kizmaylov@2u.com>
2024-06-10 15:03:14 +0300

    version 1.2

--- buildSrc/src/main/java/OfflineDependencies.kt
+++ buildSrc/src/main/java/OfflineDependencies.kt
@@ -1,6 +1,6 @@
 object OfflineDependencies {
-    const val versionCode = 4
-    const val versionName = "1.1"
+    const val versionCode = 5
+    const val versionName = "1.2"
     const val constraintLayout = "2.1.0"
     const val appCompat = "1.5.0"
     const val recyclerView = "1.2.1"
