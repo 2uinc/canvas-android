9f16f560637f5755d659ff54c49f8ef08fe26b75
srajupusapati <srajupusapatirl-sonata@2u.com>
2025-10-28 16:48:58 +0530

    Updated versions

--- apps/buildSrc/src/main/java/OfflineDependencies.kt
+++ apps/buildSrc/src/main/java/OfflineDependencies.kt
@@ -1,6 +1,6 @@
 object OfflineDependencies {
-    const val versionCode = 10
-    const val versionName = "1.5"
+    const val versionCode = 11
+    const val versionName = "1.6"
     const val constraintLayout = "2.1.0"
     const val appCompat = "1.5.0"
     const val recyclerView = "1.2.1"
