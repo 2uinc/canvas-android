1ae4460f8cb7ff3c3913289bf283244269d7e61d
srajupusapati <srajupusapatirl-sonata@2u.com>
2025-11-24 11:30:13 +0530

    Update OfflineDependencies.kt

--- apps/buildSrc/src/main/java/OfflineDependencies.kt
+++ apps/buildSrc/src/main/java/OfflineDependencies.kt
@@ -1,5 +1,5 @@
 object OfflineDependencies {
-    const val versionCode = 14
+    const val versionCode = 15
     const val versionName = "1.9"
     const val constraintLayout = "2.1.0"
     const val appCompat = "1.5.0"
